5b8403ae586825938e8f2b1a6674a4add00b6e6a
EtozheAng <pavel.khisamutdinov@yandex.ru>
2023-09-06 10:51:50 +0300

    Исправил баг добавления класса

--- src/components/bem-blocks/header-mobile/header-mobile.js
+++ src/components/bem-blocks/header-mobile/header-mobile.js
@@ -1,7 +1,11 @@
 ;(function () {
   const burger = document.querySelector('.header-mobile__button')
   burger.addEventListener('click', () => {
-    document.body.classList.toggle('body-lock')
+    if (!burger.classList[1]) {
+      document.body.classList.add('body-lock')
+    } else {
+      document.body.classList.remove('body-lock')
+    }
     burger.classList.toggle('header-mobile__button--active')
   })
   removeActiveClass(burger, 'header-mobile__button--active')
